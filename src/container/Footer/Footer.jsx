d5c8cdc7eb375ad5e2b792d9a5a158989470b236
Nima Ferdowsi <nimalangariferdowsi1383@gmail.com>
2022-02-12 14:13:53 +0330

    first

---
+++ src/container/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+
+import { FooterOverlay, Newsletter } from '../../components';
+import { images } from '../../constants';
+import './Footer.css';
+
+const Footer = () => (
+  <div className="app__footer section__padding" id="login">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">14 S 20rd St, New York, NY 10020, USA</p>
+        <p className="p__opensans">+1 530-918-8327</p>
+        <p className="p__opensans">+98 902-449-9303</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <a target='_blank' href='https://www.instagram.com/full_dev_/'>
+          <FiInstagram  />
+
+          </a>
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">10:00 am - 11:00 pm</p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      </div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2022 Rise All Rights reserved.</p>
+    </div>
+
+  </div>
+);
+
+export default Footer;
